9b151c188ebffd7f98450dac1d7ddbfe39e7f88b
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2023-03-29 16:26:11 -0600

    Start adding recipe list

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,29 @@ import './bulma.min.css';
 import './App.css';
 
 function App() {
+	const [recipes, setRecipes] = useState([{id: 1, name: 'Blue Milk'}]);
+
+	function handleRecipeItemClick(event) {
+		console.log(event.target.textContent);
+	}
+
 	return (
-		<>
+		<div id='main'>
 			<h1 className='title'>React Recipe App</h1>
-		</>
+			<button className='button'>Add New</button>
+			<div className='menu'>
+				<p className='menu-label'>
+					Your Recipes
+				</p>
+				<ul className='menu-list'>
+					{recipes.map(recipe => (
+						<li key={recipe.id}>
+							<a onClick={handleRecipeItemClick}>{recipe.name}</a>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
 	);
 }
 
