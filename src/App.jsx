00b71f98062a1aa2e1d17245bdb2fbb90c635a53
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2023-03-28 18:58:24 -0600

    Add Bulma

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState, useEffect } from 'react';
+import './bulma.min.css';
 import './App.css';
 
 function App() {
-	
+	return (
+		<>
+			<h1 className='title'>React Recipe App</h1>
+		</>
+	);
 }
 
 export default App;
