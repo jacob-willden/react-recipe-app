a20dbfff8d9140bc29de09b545b9e377e4b3aa49
Jacob Willden <67853915+jacob-willden@users.noreply.github.com>
2025-01-03 15:41:37 -0700

    Add JavaScript license table for LibreJS

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	// © 2024 Rafał Goławski, released under the Universal Permissive License version 1.0
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: "assets/[name].js"
+			}
+		}
+	}
 })
